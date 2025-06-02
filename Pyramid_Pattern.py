af49d9907fe44dd80277d427a7aecd32211f1ede
Teja-Arepally <arepallyteja@gmail.com>
2025-06-02 14:31:57 +0530

    Add files via upload

---
+++ Pyramid_Pattern.py
@@ -0,0 +1,8 @@
+def pyramid(n):
+    for i in range(n):
+        for j in range(i,n):
+            print(' ',end='')
+        for j in range(i+1):
+            print('*',end=' ')
+        print()
+pyramid(6)                
